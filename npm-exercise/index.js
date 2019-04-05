a997e6d1d59db01172f162b9c939664dfd4d7e3e
Lzduque <lzduque@hotmail.com>
2019-04-05 20:31:20 +0000

    mocking text - started and finish exercise 00 and 01

--- npm-exercise/index.js
+++ npm-exercise/index.js
@@ -1,6 +1,6 @@
 var chalk = require("chalk");
 
-var message = chalk.rgb(255, 136, 0)("Hello ") + chalk.red("World");
+var message = chalk.pulse("Hello ") + chalk.red("World");
 console.log(message);
 
 // console.log(chalk);
