312d2f078763e0fe363208a61ba5470e1923bded
Lzduque <lzduque@hotmail.com>
2019-04-05 21:15:15 +0000

    solved exercise 02

--- assessment-exam-student/answers/02.js
+++ assessment-exam-student/answers/02.js
@@ -18,12 +18,65 @@
  *    4
  */
 function median(arr) {
-  /* IMPLEMENT ME */
+  var result;
+
+  var isEven = function(num) {
+    if (num % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  var sortArr = function(arr) {
+    return arr.sort(function(a, b) {
+      return a - b;
+    });
+  };
+
+  function round(number) {
+    return Math.round(number * 100) / 100;
+  }
+
+  var mean = function(num1, num2) {
+    var sum = num1 + num2;
+    return sum / 2;
+  };
+
+  var roundDown = function(num) {
+    return num - 0.5;
+  };
+
+  var sortedArr = sortArr(arr);
+
+  var isArrEven = isEven(arr.length);
+
+  var halfArrLength = sortedArr.length / 2;
+
+  if (isArrEven === true) {
+
+    // console.log("array: ",sortedArr);
+    // console.log("halfArrLength: ",halfArrLength);
+
+    return round(mean(sortedArr[halfArrLength], sortedArr[(halfArrLength) - 1]));
+
+  } else {
+
+    // console.log("array: ",sortedArr);
+    // console.log("halfArrLength: ",halfArrLength);
+    // console.log("rounded down: ",roundDown(halfArrLength));
+    // console.log("the corresponding num: ",sortedArr[roundDown(halfArrLength)]);
+
+    return round(sortedArr[roundDown(halfArrLength)]);
+
+  }
+
 }
 
-function round(number) {
-  return Math.round(number * 100) / 100;
-}
+// console.log(median([2,1,3]));
+// console.log(median([1,2,3]));
+// console.log(median([1,2,3,4]));
+// console.log(median([3,4,1,2]));
 
 // Don't change below:
 
